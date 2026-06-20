7dc84561e8562fece452ce833651909ef8589e4f
balaji <balubongu185@gmail.com>
2026-06-20 20:10:53 +0530

    security group

--- sg.tf
+++ sg.tf
@@ -1,6 +1,5 @@
 resource "aws_security_group" "main"{
-  count=length(var.sg_names)
-  name        = var.sg_names[count.index]
+  name        = var.sg_name
   description = var.description
   vpc_id      = var.vpc_id
 
@@ -15,7 +14,7 @@ resource "aws_security_group" "main"{
     var.sg_tags,
     local.common_tags,
     {
-      Name = "${local.common_name}-${var.sg_names[count.index]}"
+      Name = "${local.common_name}-${var.sg_name}"
     }
   )
 }
--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "name"{
 }
 
 variable "sg_names"{
-    type=list
+    type=string
 }
 
 variable "description"{
